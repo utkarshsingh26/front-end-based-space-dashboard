567fb6b7a3f7cef5a4ad07bf089a19179ef51eed
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2024-12-29 18:33:03 +0530

    heatmap added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import 'leaflet.heat'; 
 
-function Map({ markers = [] }) { // Default to an empty array if markers is undefined
+function Map({ markers = [] }) {
     const position = [51.505, -0.09]; // Default map position
+
+
+    const heatmapData = markers.map(marker => [marker.lat, marker.long]);
+
+    function AddHeatmapLayer() {
+        const map = useMap();
+        useEffect(() => {
+            if (heatmapData.length > 0) {
+
+                L.heatLayer(heatmapData, { radius: 25 }).addTo(map);
+            }
+        }, [map, heatmapData]); 
+
+        return null;
+    }
 
     return (
         <div style={{ height: '100vh', width: '100%' }}>
@@ -16,15 +33,18 @@ function Map({ markers = [] }) { // Default to an empty array if markers is unde
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
+                
+                <AddHeatmapLayer />
+
                 {markers.length > 0 ? (
                     markers.map((marker, index) => (
                         <Marker key={index} position={[marker.lat, marker.long]}>
                             <Popup>
-                            <div>
+                                <div>
                                     <strong>Title:</strong> {marker.title} <br />
                                     <strong>Summary:</strong> {marker.summary} <br />
                                     <strong>URL:</strong> <a href={marker.url} target="_blank" rel="noopener noreferrer">{marker.url}</a>
-                            </div>
+                                </div>
                             </Popup>
                         </Marker>
                     ))
