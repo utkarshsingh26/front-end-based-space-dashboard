1306680c8573f69d00e69e393d435e1b38bc22b2
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2024-12-29 18:09:09 +0530

    finalResult array working fine

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -7,6 +7,8 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import Papa from 'papaparse';
+
 
 function SearchBox() {
 
@@ -18,7 +20,31 @@ function SearchBox() {
         const formattedStartDate = startDate ? startDate.format('MM-DD-YYYY') : 'Not selected';
         const formattedEndDate = endDate ? endDate.format('MM-DD-YYYY') : 'Not selected';
         console.log(keyword, formattedStartDate, formattedEndDate)
+        loadCSV(keyword, formattedStartDate, formattedEndDate);
     }
+
+    const finalResult = [];
+
+    function loadCSV(keyword, from, to) {
+        const csvUrl = new URL('./spaceheatmap_data_f47.csv', import.meta.url); 
+        fetch(csvUrl)
+          .then(response => response.text())
+          .then(csvString => {
+            Papa.parse(csvString, {
+              header: true,
+              complete: results => {
+                let csv = results.data;
+                for(let i=0; i<csv.length; i++){
+                  if((csv[i].locations == keyword) && (new Date(csv[i].date)>=new Date(from) && new Date(csv[i].date)<=new Date(to))){
+                    finalResult.push(csv[i]);
+                  }
+                }
+                console.log(finalResult)
+              }
+            });
+          })
+          .catch(error => console.error('Error fetching or parsing CSV:', error));
+      }
 
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
